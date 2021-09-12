573fd37ca09616bb2319fa59e0cd1738cea1338e
Alex Riley <riley@sfp.net>
2021-09-12 18:06:28 -0400

    feat(lol): add in tiers & divisions

--- commands/lolProfile.js
+++ commands/lolProfile.js
@@ -16,20 +16,43 @@ module.exports = {
       .then(function (response) {
         // Success Handling
 
-        // Set up Embed
-        const summonerProfile = new MessageEmbed()
-          .setColor('#0099ff')
-          .setTitle(response.data.name)
-          .addFields(
-            { name: 'Summoner Level', value: response.data.summonerLevel.toString() }
-          )
-					.setThumbnail(`http://ddragon.leagueoflegends.com/cdn/11.18.1/img/profileicon/${summoner.profileIconId}.png`)
-          .setTimestamp()
-          .setFooter('Pulled using the DEX Bot');
-        interaction.reply({embeds: [summonerProfile], ephmeral: false});
+        const summoner = response.data;
+
+        axios.get(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summoner.id.toString()}?api_key=${lolDevToken}`)
+          .then(function (response) {
+            // Set up Embed
+
+              var rankedSolo = 'N/A';
+              var rankedFlex = 'N/A';
+              if(response.data[0]) {
+                rankedSolo = `Tier: ${response.data[0].tier} ${response.data[0].rank} [W/L: ${response.data[0].wins.toString()}/${response.data[0].losses.toString()}]`;
+              }
+
+              if(response.data[1]) {
+                rankedFlex = `Tier: ${response.data[1].tier} ${response.data[1].rank} [W/L: ${response.data[1].wins.toString()}/${response.data[1].losses.toString()}]`;
+              }
+
+              const summonerProfile = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle(summoner.name)
+                .addFields(
+                  { name: 'Summoner Level', value: summoner.summonerLevel.toString() },
+                  { name: 'Ranked Solo', value: rankedSolo },
+                  { name: 'Ranked Flex', value: rankedFlex }
+                )
+                .setThumbnail(`http://ddragon.leagueoflegends.com/cdn/11.18.1/img/profileicon/${summoner.profileIconId}.png`)
+                .setTimestamp()
+                .setFooter('Pulled using the DEX Bot');
+              interaction.reply({embeds: [summonerProfile], ephmeral: false});
+          })
+          .catch(function (error) {
+            interaction.reply({content: 'Unable to pull summoner data.', ephmeral: true});
+            console.log(error);
+          });
       })
       .catch(function (error) {
         // Error Handling
+        interaction.reply({content: 'Unable to find a summoner with that name.', ephmeral: true});
         console.log(error);
       });
 		//
