94a3a83d31460689d3426b041b24e7eb6a69ea78
Alex Riley <riley@sfp.net>
2021-09-19 01:47:40 -0400

    fix(backups): create backups directory if not exists

--- jobs/createDatabaseBackup.js
+++ jobs/createDatabaseBackup.js
@@ -5,7 +5,15 @@ const path = require('path');
 module.exports = function() {
     // Cron job to create database backup daily
     var createDatabaseBackup = new cron.CronJob('0 0 * * *', () => {
+
       const currentDate = new Date().toISOString().split('T')[0];
+
+      // Create directory if not exist
+      if(!fs.existsSync(path.join(__dirname, '../backup'))) {
+        console.log('Creating backup directory');
+        fs.mkdirSync(path.join(__dirname, '../backup'));
+      }
+      // Make backup
       fs.copyFile(path.join(__dirname, '../database.sqlite'), path.join(__dirname, `../backup/database_${currentDate}.sqlite`), (err) => {
         if(err) {
           console.log('FATAL: Database failed to backup.\n' + err);
