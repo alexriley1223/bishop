6f7450fbaf919227b19b31c60889a7fd48602c3a
Alex Riley <riley@sfp.net>
2021-09-21 18:20:33 -0400

    feat: unregister slash commands script

---
+++ unregister-commands.js
@@ -0,0 +1,38 @@
+// Please refactor this awful mess
+// Only used for dev testing but still
+
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
+
+var commands = [];
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+  try {
+
+    var commandList = await rest.get(
+      Routes.applicationGuildCommands(clientId, guildId)
+    );
+
+    commandList.forEach(function(command){
+      commands.push(command.id);
+    });
+
+  } catch (error) {
+    console.error(error);
+  } finally {
+    commands.forEach(function(command){
+      try {
+        console.log(`Attempting to unregister command ${command}`);
+        rest.delete(Routes.applicationGuildCommand(clientId, guildId, command)).then(function(res){
+          console.log(`Successfully unregistered command ${command}`);
+        });
+
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  }
+})();
