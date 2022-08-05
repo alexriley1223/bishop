390dca4761f16db6c3cc56dd63d00771ba851f72
Alex Riley <alexriley1223@gmail.com>
2022-08-04 20:12:51 -0400

    feat: overhaul of randomizer

--- README.md
+++ README.md
@@ -1,42 +1,99 @@
+<div align="center">
+	<br />
+	<p>
+		<img src="https://git.alexriley.me/repo-avatars/12-5bf0c8fdfc11c47e193cd60a18a1a7b7" width="250" alt="Bishop Discord Bot" />
+	</p>
+	<p>
+		<img src="https://img.shields.io/discord/604872185391087629?style=for-the-badge" alt="DEX Discord Server" />
+</div>
 
-# Bishop Discord Bot
-Feature-packed discord bot for the https://d3x.me/ Discord server.
+## About
+Bishop is an all-in-one, self hosted Discord bot for developers and tinkerers. Built using <a href="https://discord.js.org/">discord.js</a>, Bishop is easily expandable to suite your server's needs.
+## Features
+- Dynamic and expandable command, event, database, and job system
+- Radio streams from YouTube
+- Points system based on user engagement with your server
+- Full Discord API coverage with usage of discord.js
 
-## Config Variables
-To run this project, you will need to add the following environment variables to your configi/xxxxxx.json files
+### Modules
+**These are the modules available out of the box with Bishop**
+- General: Generic server commands
+- Lol: League of legends specific commands
+- Points: A dynamic user points system
+- Radio: YouTube stream radio with play/pause/queueing
+- Util: More generic server commands, more Admin focused
+
+## Installation
+**Requires Node.js >= 16.9.0**
+
+### Acquire packages
+```sh-session
+npm install
+```
+<br>
+
+### Populate all config files in config/*
+<br>
 
 **api.json**
-`lolDevToken` - If using LoL API
-
-**bot.json**
-`clientId` - Bot User ID
-
-`guildId` - Server ID
-
-`token` - Discord Bot Secret
-
-`color` - Color to use for embedded messages
-
-**channels.json**
-`musicChannelId` - ID of channel to funnel all music commands through
-
-`gameUpdatesChannelId` - ID of channel to post casino/game updates to
-
-`announcementsChannelId` - ID of channel for general announcements
-
-`afkChannelId` - ID of AFK channel
-
-
-## Run Locally
-
-Run the included upgrade command
-```bash
-  npm run upgrade
 ```
+API Tokens
 
-Run Client
-```bash
-  node app.js
+{
+  "lolDevToken": "RGAPI-12345", // LoL API Token if using LoL API
+  "alphaToken": "1M5AHBO6OVB15SRW" // Alpha token if using stock game
+}
+```
+**bot.json**
+```
+Bot specific information
+
+{
+  "clientId": "Your Bot Client ID",
+  "guildId": "Your Guild ID",
+  "token": "Bot Token/Secret",
+  "color": "#FFFFFF", // Color to use for Bot Embeds / Global
+  "name": "Bishop" // Name of your Bot
+}
+```
+**channels.json**
+```
+Channel IDs of associated channels
+
+{
+  "musicChannelId": "12345", // Radio updates
+  "gameUpdatesChannelId": "12345", // Points game updates
+  "announcementsChannelId": "12345", // Server announcements
+  "afkChannelId": "12345", // AFK Channel
+  "botUpdatesChannel": "12345" // Bot updates
+}
+```
+**modules.json**
+```
+Which modules to have enabled
+
+{
+  "general": true,
+  "lol": true,
+  "points": true,
+  "radio": true,
+  "util": true
+}
+```
+**roles.json**
+```
+List of roles and point thresholds for point-acquired server ranks. Populate as needed
+
+"roles": [
+    { "tier": Order, "id": "RoleId", "amount": minimumPoints },
+    { "tier": 2, "id": "12345", "amount": 200 },
+  ]
+```
+### Deploy
+```sh-session
+node deploy-commands.js
+node initialize-database.js
+node app.js
 ```
 
 ## Prepare for Deployment
--- commands/lol/lolRandomizer.js
+++ commands/lol/lolRandomizer.js
@@ -1,24 +1,66 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, AttachmentBuilder } = require('@discordjs/builders');
 const { color, name } = require('@config/bot.json');
-const { MessageEmbed } = require('discord.js');
 
 const fs = require('fs');
+const path = require('path');
+const { createCanvas, loadImage} = require('canvas');
 
 /* Static JSON */
 let championData = require('@assets/lol/constants/champion.json');
 let itemData = require('@assets/lol/constants/items.json');
 let summonerData = require('@assets/lol/constants/summoners.json');
+let runeData = require('@assets/lol/constants/runes.json');
+const { join } = require('path');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('lolrandomizer')
-		.setDescription('Random fun!'),
+		.setDescription('Random fun!')
+        .addStringOption(option =>
+            option.setName('role')
+                .setDescription('Your role on the Rift')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'ADC', value: 'adc' },
+                    { name: 'Support', value: 'support' },
+                    { name: 'Mid', value: 'mid' },
+                    { name: 'Jungle', value: 'jungle' },
+                    { name: 'Top', value: 'top' },
+                    { name: 'Fill', value: 'fill' }
+                )),
 	async execute(interaction) {
+        await interaction.deferReply();
 
+        /* Deployed Arrays*/
         let items = [];
-        let boots = [];
         let summonerSpells = [];
+        let abilities = [];
         let champion;
+        let runeTree = [];
+        let mods = [];
+
+        /* Utility */
+        const appRoot = path.dirname(require.main.filename);
+        let boots = [];
+        const selectedRole = interaction.options.getString('role');
+        let allowedAbilities = ['Q', 'W', 'E'];
+        const statMods = [
+            [   
+                { name: 'Adaptive Force', picture: 'StatModsAdaptiveForceIcon' },
+                { name: 'Attack Speed', picture: 'StatModsAttackSpeedIcon' },
+                { name: 'Ability Haste', picture: 'StatModsCDRScalingIcon' }
+            ],
+            [
+                { name: 'Adaptive Force', picture: 'StatModsAdaptiveForceIcon' },
+                { name: 'Armor', picture: 'StatModsArmorIcon' },
+                { name: 'Magic Resist', picture: 'StatModsMagicResIcon.MagicResist_Fix' }
+            ],
+            [
+                { name: 'Health', picture: 'StatModsHealthScalingIcon' },
+                { name: 'Armor', picture: 'StatModsArmorIcon' },
+                { name: 'Magic Resist', picture: 'StatModsMagicResIcon.MagicResist_Fix' }
+            ]
+        ];
         const mythicItems = [
             'Crown of the Shattered Queen',
             'Divine Sunderer',
@@ -46,13 +88,23 @@ module.exports = {
             'Trinity Force',
             'Turbo Chemtank'
         ];
-        
-        const randomizerEmbed = new MessageEmbed()
-            .setColor(color)
-            .setTitle(`League Randomizer`)
-            .setDescription('')
-            .setTimestamp()
-            .setFooter(`Pulled using the ${name} Bot`);
+
+
+        /**
+         * START THE FUN
+         */
+
+        // Get rune tree
+        getRandomRunePrimary();
+        getRandomRuneSecondary();
+        getRandomStatMods();
+
+        // Get a random leveling order
+        for(var i = 0; i < 3; i++) {
+            var random = allowedAbilities[Math.floor(Math.random() * allowedAbilities.length)];
+            abilities.push(random);
+            allowedAbilities.splice(allowedAbilities.indexOf(random), 1);
+        }
 
         // Get array of boots
         for (const [key, value] of Object.entries(itemData.data)) {
@@ -71,7 +123,6 @@ module.exports = {
 
         // Random champion
         champion = getRandomChampion();
-        randomizerEmbed.setThumbnail('https://ddragon.leagueoflegends.com/cdn/12.14.1/img/champion/'+champion.name+'.png');
 
         // Give 'er a random boot
         items.push(boots[Math.floor(Math.random() * boots.length)]);
@@ -89,47 +140,249 @@ module.exports = {
             summonerSpells.push(getRandomSummonerSpell());
         }
 
-        randomizerEmbed.addField('Champion', champion.name);
-        randomizerEmbed.addField('\u200B', '\u200B');
 
-        summonerSpells.forEach(function(spell) {
-            randomizerEmbed.addField('Summoner Spell', spell.name, true);
+        /**
+         * Generate the embed image
+         */
+
+        const width = 600;
+        const height = 1000;
+
+        const canvas = createCanvas(width, height);
+        const context = canvas.getContext('2d');
+        context.font = 'bold 45pt Menlo'
+        context.textAlign = 'center'
+        context.fillStyle = '#fff'
+
+        // Load base image
+        await loadImage(appRoot + '/assets/lol/constants/template.png').then(image => {
+            context.drawImage(image, 0, 0, 600, 1000);
         });
 
-        randomizerEmbed.addField('\u200B', '\u200B');
+        // Load champion Icon
+        await loadImage('https://ddragon.leagueoflegends.com/cdn/12.14.1/img/champion/'+ champion.image.full).then(image => {
+            context.drawImage(image, 225, 20, 150, 150);
+        });
+        context.fillText(champion.name, 300, 250);
 
-        items.forEach(function(item, index) {
-            if(index == 0) {
-                randomizerEmbed.addField('Boots', item.name);
-                randomizerEmbed.addField('\u200B', '\u200B');
-            } else if (index == 1) {
-                randomizerEmbed.addField('Mythic', item.name);
-                randomizerEmbed.addField('\u200B', '\u200B');
-            } else {
-                randomizerEmbed.addField('Item', item.name,true);
-            }
+        // Load summs
+        await loadImage(appRoot + '/assets/lol/constants/spell/' + summonerSpells[0].image['full']).then(image => {
+            context.drawImage(image, 20, 20, 75, 75);
+        });
+        await loadImage(appRoot + '/assets/lol/constants/spell/' + summonerSpells[1].image['full']).then(image => {
+            context.drawImage(image, 500, 20, 75, 75);
+        });
+
+        // Load Runes (NON DYNAMIC)
+
+        // Col 1 (Left Runes)
+        await loadImage(appRoot + '/assets/lol/constants/' + runeTree[0].keystones[0].icon).then(image => {
+            context.drawImage(image, 50, 400, 50, 50);
+        });
+        await loadImage(appRoot + '/assets/lol/constants/' + runeTree[0].keystones[1].icon).then(image => {
+            context.drawImage(image, 50, 475, 50, 50);
+        });
+        await loadImage(appRoot + '/assets/lol/constants/' + runeTree[0].keystones[2].icon).then(image => {
+            context.drawImage(image, 125, 400, 50, 50);
+        });
+        await loadImage(appRoot + '/assets/lol/constants/' + runeTree[0].keystones[3].icon).then(image => {
+            context.drawImage(image, 125, 475, 50, 50);
+        });
+
+        // Col 2 (Right Runes)
+        await loadImage(appRoot + '/assets/lol/constants/' + runeTree[1].keystones[0].icon).then(image => {
+            context.drawImage(image, 300, 400, 50, 50);
+        });
+        await loadImage(appRoot + '/assets/lol/constants/' + runeTree[1].keystones[1].icon).then(image => {
+            context.drawImage(image, 300, 475, 50, 50);
+        });
+
+        // Col 3 (Stat Mods)
+        await loadImage(appRoot + '/assets/lol/constants/mods/' + mods[0].picture +'.png').then(image => {
+            context.drawImage(image, 550, 400, 50, 50);
+        });
+        await loadImage(appRoot + '/assets/lol/constants/mods/' + mods[1].picture +'.png').then(image => {
+            context.drawImage(image, 550, 475, 50, 50);
+        });
+        await loadImage(appRoot + '/assets/lol/constants/mods/' + mods[2].picture +'.png').then(image => {
+            context.drawImage(image, 550, 550, 50, 50);
+        });
+
+        // Load Items (NON DYNAMIC RIGHT NOW)
+        await loadImage(appRoot + '/assets/lol/constants/item/' + items[0].image['full']).then(image => {
+            context.drawImage(image, 50, 650, 75, 75);
+        });
+        await loadImage(appRoot + '/assets/lol/constants/item/' + items[1].image['full']).then(image => {
+            context.drawImage(image, 250, 650, 75, 75);
+        });
+        await loadImage(appRoot + '/assets/lol/constants/item/' + items[2].image['full']).then(image => {
+            context.drawImage(image, 450, 650, 75, 75);
+        });
+        await loadImage(appRoot + '/assets/lol/constants/item/' + items[3].image['full']).then(image => {
+            context.drawImage(image, 50, 750, 75, 75);
+        });
+        await loadImage(appRoot + '/assets/lol/constants/item/' + items[4].image['full']).then(image => {
+            context.drawImage(image, 250, 750, 75, 75);
+        });
+        await loadImage(appRoot + '/assets/lol/constants/item/' + items[5].image['full']).then(image => {
+            context.drawImage(image, 450, 750, 75, 75);
+        });
+
+
+        // Ability Order
+        context.font = 'bold 35pt Menlo'
+        let joinedAbilities = abilities.join(' -> ');
+        context.fillText(joinedAbilities, 300, 950);
+
+        const buffer = canvas.toBuffer('image/png');
+        let randString = (Math.random() + 1).toString(36).substring(7);
+        await fs.writeFileSync(appRoot + "/tmp/"+randString+".png", buffer);
+
+        await interaction.editReply({ files: [appRoot + "/tmp/" + randString + ".png"]});
+
+        /**
+         * Helper Functions
+         */
+
+        // Get a random primary rune
+        function getRandomRunePrimary() {
+            var runeObj = {};
+            var randomRune = runeData[Math.floor(Math.random() * 5)];
             
-        });
+            runeObj['name'] = randomRune.name;
+            runeObj['icon'] = randomRune.icon;
+            runeObj['keystones'] = [];
 
-        interaction.reply({ embeds: [randomizerEmbed], ephemeral: false });
+            for(var i = 0; i < 4; i++) {
+                let keystone = randomRune.slots[i].runes[Math.floor(Math.random() * randomRune.slots[i].runes.length)];
+                
+                runeObj['keystones'].push({
+                    name: keystone.name,
+                    icon: keystone.icon
+                });
+            }
 
+            runeTree.push(runeObj);            
+        }
+
+        // Get a random secondary Rune
+        function getRandomRuneSecondary() {
+            var runeObj = {};
+            var randomRune = runeData[Math.floor(Math.random() * 5)];
+
+            // No duplicate
+            if(randomRune.name == runeTree[0].name) {
+                return getRandomRuneSecondary();
+            }
+
+            runeObj['name'] = randomRune.name;
+            runeObj['icon'] = randomRune.icon;
+            runeObj['keystones'] = [];
+
+            var excludedSlot;
+            for(var i = 0; i < 2; i++) {
+                // Always 4 slots
+                var randomKeystoneSlot = Math.floor(Math.random() * 3) + 1;
+                if(excludedSlot) {
+                    while(randomKeystoneSlot == excludedSlot) {
+                        randomKeystoneSlot = Math.floor(Math.random() * 3) + 1;
+                    }
+                }
+                
+                var randomRuneLength = randomRune.slots[randomKeystoneSlot].runes.length;
+                let keystone = randomRune.slots[randomKeystoneSlot].runes[Math.floor(Math.random() * randomRuneLength)];
+                
+                if(!excludedSlot) {
+                    excludedSlot = randomKeystoneSlot;
+                }
+
+                // Make sure keystone
+                runeObj['keystones'].push({
+                    name: keystone.name,
+                    icon: keystone.icon
+                });
+            }
+
+            runeTree.push(runeObj);
+        }
+
+        // Get random rune stat mods
+        function getRandomStatMods() {
+            statMods.forEach(modRow => {
+                let randomMod = modRow[Math.floor(Math.random() * 2)];
+                mods.push({ name: randomMod.name, picture: randomMod.picture });
+            });
+        }
+
+        // Get a random valid champion
         function getRandomChampion() {
             var keys = Object.keys(championData.data);
-            return championData.data[keys[ keys.length * Math.random() << 0]];    
+            let randomChampion = championData.data[keys[ keys.length * Math.random() << 0]];
+           
+            var validRole = false;
+
+            switch(selectedRole) {
+                case 'adc':
+                    if(randomChampion.tags.includes('Marksman')) {
+                        validRole = true;
+                    }
+                    break;
+
+                case 'mid':
+                    if(randomChampion.tags.includes('Assassin') || randomChampion.tags.includes('Mage') || randomChampion.tags.includes('Support')) {
+                        validRole = true;
+                    }
+                    break;
+
+                case 'top':
+                    if(randomChampion.tags.includes('Fighter') || randomChampion.tags.includes('Tank')) {
+                        validRole = true;
+                    }
+                    break;
+
+                case 'jungle':
+                    if(randomChampion.tags.includes('Fighter') || randomChampion.tags.includes('Assassin') || randomChampion.tags.includes('Tank')) {
+                        validRole = true;
+                    }
+                    break;
+
+                case 'support':
+                    if(randomChampion.tags.includes('Support') || randomChampion.tags.includes('Mage')) {
+                        validRole = true;
+                    }
+                    break;
+
+                case 'fill':
+                    validRole = true;
+                    break;
+            }
+
+            if(!validRole) {
+                return getRandomChampion();
+            }
+
+            return randomChampion;
         }
+
         // Get a random valid summoner spell
         function getRandomSummonerSpell() {
 
+            // Give jungler smite every time
+            if(selectedRole == 'jungle' && summonerSpells.length > 0) {
+                return summonerData.data['SummonerSmite'];
+            }
+            
             var keys = Object.keys(summonerData.data);
             let newSumm = summonerData.data[keys[ keys.length * Math.random() << 0]];
 
-            let isOnSR = false
+            
             if(summonerSpells.length > 0) {
                 if(summonerSpells[0].name == newSumm.name) {
                     return getRandomSummonerSpell();
                 }
             }
 
+            let isOnSR = false
             newSumm.modes.forEach(function(x) {
                 if(x == 'CLASSIC') {
                     isOnSR = true;
@@ -156,6 +409,13 @@ module.exports = {
                 }
             });
 
+            // No duplicates
+            items.forEach(function(existing) {
+                if(newItem.name == existing.name) {
+                    return getRandomItem(mythic);
+                }
+            });
+
             // Only full items
             if(newItem.depth != 3) {
                 return getRandomItem(mythic);
@@ -178,8 +438,8 @@ module.exports = {
 
             // Map check
             let isOnSR = false;
-            Object.keys(newItem.maps).forEach(function(map) {
-                if(map == '11') {
+            Object.keys(newItem.maps).forEach(function(map, mapIndex) {
+                if(map == '11' && newItem.maps[map] == true) {
                     isOnSR = true;
                 }
             });
