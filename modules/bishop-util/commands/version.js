078169629d0b40df42426c48cf4f79dce143c501
Alex Riley <alexriley1223@gmail.com>
2023-10-21 14:31:29 -0400

    fixes #18

--- modules/bishop-util/commands/version.js
+++ modules/bishop-util/commands/version.js
@@ -1,15 +1,15 @@
 const BishopCommand = require('@classes/BishopCommand');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const gitVersion = require('git-tag-version');
 const { getParentDirectoryString } = require('@helpers/utils');
 const { commands } = require('../config.json');
+const { version } = require('@root/package.json');
 
 module.exports = new BishopCommand({
 	enabled: commands[getParentDirectoryString(__filename, __dirname)],
 	data: new SlashCommandBuilder().setName('version').setDescription('Show current bot version'),
 	execute: async function(interaction) {
 		interaction.reply({
-			content: `${interaction.client.bishop.name} is currently running on v${gitVersion()}!`,
+			content: `${interaction.client.bishop.name} is currently running on v${version}!`,
 			ephemeral: true,
 		});
 	},
