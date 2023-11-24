7267e15b74417ff1d9072175a4692a510e03ed32
Alex Riley <alexriley1223@gmail.com>
2023-11-24 01:07:51 -0500

    feat: support timezone overrides

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require('module-alias/register');
+const { timezone } = require('@config/bot.json');
 
 const BishopClient = require('@classes/BishopClient');
 const client = new BishopClient();
+
+process.env.TZ = timezone;
 
 client.boot();
 
